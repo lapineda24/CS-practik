1ee53292d0d98da69d67643e2a812c1699b18a57
Vera <student@C251U09>
2020-09-04 11:18:16 +0300

    2

---
+++ pr01/01/01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+
+namespace CreateStruct
+{
+  class Program
+  {
+    struct Person
+    {
+      public string firstName;
+      public string lastName;
+      public int age;
+      public Person(string _firstName, string _lastName, int _age)
+      {
+        firstName = _firstName;
+        lastName = _lastName;
+        age = _age;
+      }
+      public override string ToString()
+      {
+        return firstName + " " + lastName + ", age " + age;
+      }
+    }
+    static void Main(string[] args)
+    {
+      Person p = new Person("Вера", "Воробьева", 19);
+      Console.WriteLine(p);
+    }
+  }
+}
